6cd6402df71821464aabe04c6a683e0c9ec74c12
haa <haa>
2019-11-22 14:06:07 +0100

    Call usecase in test

--- core/src/test/kotlin/ch/haspra/minimi/usecases/tracking_information/ShowTrackingInformationTest.kt
+++ core/src/test/kotlin/ch/haspra/minimi/usecases/tracking_information/ShowTrackingInformationTest.kt
@@ -1,13 +1,16 @@
 package ch.haspra.minimi.usecases.tracking_information
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import kotlin.test.assertTrue
 
 class ShowTrackingInformationTest {
 
     @Test
-    fun testsWork() {
-        assertTrue(true)
+    fun trackingInformation_is_compiled_correctly() {
+        val showTrackingInformation = ShowTrackingInformation()
+        val trackingInformation = showTrackingInformation.invoke()
+
+        assertEquals(1000.0F, trackingInformation.pressure.value)
     }
 }
 
