b4dcbc83b958589a8fc354f5df575aaac48dc108
haa <haa>
2019-11-23 13:25:57 +0100

    Split core into application and domain

--- core/src/main/kotlin/ch/haspra/minimi/domain/Pressure.kt
+++
@@ -1,3 +0,0 @@
-package ch.haspra.minimi.domain
-
-data class Pressure(val value: Float)
--- core/src/main/kotlin/ch/haspra/minimi/usecases/tracking_information/port/TrackingInformation.kt
+++
@@ -1,5 +0,0 @@
-package ch.haspra.minimi.usecases.tracking_information.port
-
-import ch.haspra.minimi.domain.Pressure
-
-data class TrackingInformation(var pressure: Pressure)
